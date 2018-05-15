1e316eaeb5d13c385ad8e18e5440864c1fbb8abe
Pascal Lesage <pascal.lesage@polymtl.ca>
2018-05-14 23:36:37 -0400

    Initial commit

---
+++ RoWer/__init__.py
@@ -0,0 +1,1 @@
+from RoWer.RoWer import *
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='RoWer',
+    version="0",
+    packages=["RoWer"],
+    package_data={'constructive_geometries':
+        [
+            "data/faces.gpkg",
+            "data/faces.json"
+        ]
+    },
+    author="Pascal Lesage",
+    author_email="pascal.lesage@polymtl.com",
+    #license=""
+    url="https://github.com/PascalLesage/RoWer",
+    install_requires=['bw2data'],
+    # long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Other Audience',
+        #'Operating System :: Microsoft :: Windows',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
